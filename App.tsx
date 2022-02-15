540ef910b24d3da951dca86f51844a6c78a09fed
Arthur Bicalho <arthurbic95@gmail.com>
2022-02-14 21:23:15 -0300

    Final Commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,149 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef } from "react";
+
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  SafeAreaView,
+  Keyboard,
+} from "react-native";
+
+import LottieView from "lottie-react-native";
+import api from "./src/services/api";
 
 export default function App() {
+  const [cep, setCep] = useState("");
+  const [cepUser, setCepUser] = useState(null);
+  const inputRef = useRef(null);
+
+  function limpar() {
+    setCep("");
+    setCepUser(null);
+    inputRef.current.focus();
+  }
+
+  async function buscar() {
+    if (cep == "") {
+      alert("Digite um CEP valido ");
+      setCep("");
+      return;
+    }
+
+    try {
+      const response = await api.get(`/${cep}/json`);
+      setCepUser(response.data);
+      Keyboard.dismiss();
+    } catch (error) {
+      console.log("ERROR: " + error);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{ alignItems: "center" }}>
+        <Text style={styles.text}>Digite o cep desejado</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Ex: 30170000"
+          value={cep}
+          onChangeText={(texto) => setCep(texto)}
+          keyboardType="numeric"
+          ref={inputRef}
+        />
+      </View>
+
+      <View style={styles.areaBtn}>
+        <TouchableOpacity
+          style={[styles.botao, { backgroundColor: "#1D75CD" }]}
+          onPress={buscar}
+        >
+          <Text style={styles.botaoText}>Buscar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.botao, { backgroundColor: "#CD3E1D" }]}
+          onPress={limpar}
+        >
+          <Text style={styles.botaoText}>Limpar</Text>
+        </TouchableOpacity>
+      </View>
+
+      {cepUser ? (
+        <View style={styles.resultado}>
+          <Text style={styles.itemText}>CEP: {cepUser.cep}</Text>
+          <Text style={styles.itemText}>Logradouro: {cepUser.logradouro}</Text>
+          <Text style={styles.itemText}>Bairro: {cepUser.bairro}</Text>
+          <Text style={styles.itemText}>Cidade: {cepUser.localidade}</Text>
+          <Text style={styles.itemText}>Estado: {cepUser.uf}</Text>
+        </View>
+      ) : (
+        <View style={styles.areaLottie}>
+          <LottieView
+            source={require("./src/assets/location-animation.json")}
+            resizeMode="cover"
+            style={styles.lottie}
+            autoPlay={true}
+          />
+        </View>
+      )}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 30,
+  },
+  text: {
+    marginTop: 25,
+    marginBottom: 15,
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  input: {
+    backgroundColor: "#FFF",
+    borderWidth: 1,
+    borderColor: "#DDD",
+    borderRadius: 5,
+    width: "90%",
+    padding: 10,
+    fontSize: 18,
+  },
+  areaBtn: {
+    alignItems: "center",
+    flexDirection: "row",
+    marginTop: 15,
+    justifyContent: "space-around",
+  },
+  botao: {
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: "#FF0000",
+  },
+  botaoText: {
+    fontSize: 22,
+    color: "#FFF",
+  },
+  resultado: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  itemText: {
+    fontSize: 22,
+  },
+  lottie: {
+    width: 200,
+    height: 200,
+  },
+  areaLottie: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,23 @@
+## BUSCADOR CEP
+
+<div style='align-items: center; justify-content: center;'>
+<ul>
+ <li>O app tem a intenção de realizar a busca de um CEP valido e mostrar o CEP, logradouro, bairro, cidade e estado.</li>
+ <li>Desenvolvido em React Native, utilizando TypeScript</li>
+</ul>
+
+<div >
+<img src='./.github/assets/buscardorcepapp.gif' width='348' height='772' />
+</div>
+
+<hr>
+
+## Configurações
+
+<ul>
+<li>O app foi desenvolvido totalmente com o Expo. Link: <a href='https://docs.expo.dev/'>https://docs.expo.dev/</a> </li>
+<li>Para a aplicação rodar é necessário realizar o comando: Expo Start</li>
+<li>A API utilizada para esse projeto foi a: <a href='https://viacep.com.br/'>https://viacep.com.br/</a></li>
+<li>As dependencias utilizadas foram: <ul><li>expo install lottie-react-native</li><li>expo install axios</li><li>Link para a dependência Lottie: <a href='https://docs.expo.dev/versions/latest/sdk/lottie/'>https://docs.expo.dev/versions/latest/sdk/lottie/</a></li><li>Link para a dependência Axios: <a href='https://www.npmjs.com/package/react-native-axios'>https://www.npmjs.com/package/react-native-axios</a></li></ul></li>
+</ul>
+</div>
